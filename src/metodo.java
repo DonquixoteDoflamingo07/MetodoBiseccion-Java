ba564015ff90c02016d2d43285e1534c297d0e07
Abraham <pimpi_abraham@hotmail.com>
2025-05-16 13:40:45 -0700

    Agrega metodo de biseccion basico

---
+++ src/metodo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class metodo {
+    public static void main(String[] args) {
+        System.out.println("Metodo de Biseccion para encontrar raices de f(x) = x^3 - x - 2");
+    }
+}
